181ec9222a34bdd3da694bde6bea8a44cbee1eef
Boris Yastrebov <mrhawkofficial@gmail.com>
2019-05-18 23:55:12 +0300

    Header component update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,24 +3,37 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
 
-const Logo = styled.p``
-
-const Menu = styled.ul`
+const HeaderWrapper = styled.header`
   display: flex;
+  align-items: baseline;
+  padding: 16px 0;
+`
+
+const Logo = styled.p`
+  font-weight: 600;
+`
+
+const Menu = styled.nav`
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: flex-end;
   list-style-type: none;
 `
 
-const MenuItem = styled.li``
+const MenuItem = styled.a`
+  margin-left: 20px;
+  font-weight: 600;
+`
 
 const Header = () => (
-  <header>
+  <HeaderWrapper>
     <Logo>Yastrebov.</Logo>
     <Menu>
       <MenuItem>Projects</MenuItem>
       <MenuItem>About</MenuItem>
       <MenuItem>Contact</MenuItem>
     </Menu>
-  </header>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
