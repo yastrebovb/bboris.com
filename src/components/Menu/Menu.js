bc53ba20f48ac771b6d6a49e72fd3bcb138de10d
Boris Yastrebov <mrhawkofficial@gmail.com>
2019-05-20 00:10:22 +0300

    menu close on click

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,99 +1,10 @@
-import { Link } from "gatsby"
 import React, { Component } from "react"
 import { slide as MobileMenu } from "react-burger-menu"
-import styled from "styled-components"
-
-const DesktopMenu = styled.nav`
-  flex: 1 1 auto;
-  display: flex;
-  justify-content: flex-end;
-  padding: 38px 0;
-  list-style-type: none;
-`
-
-const Logo = styled.p`
-  font-weight: 600;
-`
-
-const MenuItem = styled(Link)`
-  margin-left: 32px;
-  margin-bottom: ${({ mobile }) => (mobile ? "36px" : "initial")};
-  color: ${({ mobile }) => (mobile ? "#ffffff" : "initial")};
-  font-weight: 600;
-  text-decoration: none;
-`
-
-const StyledMobileMenu = styled.div`
-  .bm-burger-button {
-    position: fixed;
-    width: 30px;
-    height: 25px;
-    right: 32px;
-    top: 32px;
-  }
-
-  .bm-burger-bars {
-    background: #373a47;
-  }
-
-  .bm-burger-bars-hover {
-    background: black;
-  }
-
-  .bm-cross-button {
-    right: 32px !important;
-    top: 22px !important;
-    height: 32px !important;
-    width: 32px !important;
-  }
-
-  .bm-cross {
-    width: 4px !important;
-    height: 32px !important;
-    background: #bdc3c7;
-  }
-
-  /*
-Sidebar wrapper styles
-Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
-*/
-  .bm-menu-wrap {
-    position: fixed;
-    height: 100%;
-  }
-
-  /* General sidebar styles */
-  .bm-menu {
-    background: #373a47;
-    padding: 2.5em 1.5em 0;
-    font-size: 1.15em;
-  }
-
-  /* Morph shape necessary with bubble or elastic */
-  .bm-morph-shape {
-    fill: #373a47;
-  }
-
-  /* Wrapper for item list */
-  .bm-item-list {
-    color: #b8b7ad;
-    padding: 0.8em;
-  }
-
-  /* Individual item */
-  .bm-item {
-    display: inline-block;
-  }
-
-  /* Styling of overlay */
-  .bm-overlay {
-    background: rgba(0, 0, 0, 0.3);
-  }
-`
+import { DesktopMenu, Logo, MenuItem, StyledMobileMenu } from "./style"
 
 class Menu extends Component {
   state = {
-    width: window.innerWidth,
+    width: window.screen.width,
     isOpen: false,
   }
 
@@ -105,16 +16,20 @@ class Menu extends Component {
     window.removeEventListener("resize", this.updateDimensions)
   }
 
+  handleStateChange(state) {
+    this.setState({ isOpen: state.isOpen })
+  }
+
   updateDimensions = () => {
     this.setState({
-      width: window.innerWidth,
+      width: window.screen.width,
     })
   }
 
-  toggleMenu = () => {
-    this.setState(prevState => ({
-      isOpen: !prevState.isOpen,
-    }))
+  closeMenu = () => {
+    this.setState({
+      isOpen: false,
+    })
   }
 
   render() {
@@ -123,12 +38,24 @@ class Menu extends Component {
     if (width < 768) {
       return (
         <StyledMobileMenu>
-          <MobileMenu right isOpen={isOpen} onClick={this.toggleMenu}>
-            <MenuItem to="#projects" mobile={"true"}>
+          <MobileMenu
+            right
+            isOpen={isOpen}
+            onStateChange={state => this.handleStateChange(state)}
+          >
+            <MenuItem
+              to="#projects"
+              mobile={"true"}
+              onClick={() => this.closeMenu()}
+            >
               Projects
             </MenuItem>
-            <MenuItem mobile={"true"}>About</MenuItem>
-            <MenuItem mobile={"true"}>Contact</MenuItem>
+            <MenuItem to="#about" mobile={"true"}>
+              About
+            </MenuItem>
+            <MenuItem to="#concat" mobile={"true"}>
+              Contact
+            </MenuItem>
           </MobileMenu>
         </StyledMobileMenu>
       )
---
+++ src/components/Menu/style.js
@@ -0,0 +1,90 @@
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+export const DesktopMenu = styled.nav`
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: flex-end;
+  padding: 38px 0;
+  list-style-type: none;
+`
+
+export const Logo = styled.p`
+  font-weight: 600;
+`
+
+export const MenuItem = styled(Link)`
+  margin-left: 32px;
+  margin-bottom: ${({ mobile }) => (mobile ? "36px" : "initial")};
+  color: ${({ mobile }) => (mobile ? "#ffffff" : "initial")};
+  font-weight: 600;
+  text-decoration: none;
+`
+
+export const StyledMobileMenu = styled.div`
+  .bm-burger-button {
+    position: fixed;
+    width: 30px;
+    height: 25px;
+    right: 32px;
+    top: 32px;
+  }
+
+  .bm-burger-bars {
+    background: #373a47;
+  }
+
+  .bm-burger-bars-hover {
+    background: black;
+  }
+
+  .bm-cross-button {
+    right: 32px !important;
+    top: 22px !important;
+    height: 32px !important;
+    width: 32px !important;
+  }
+
+  .bm-cross {
+    width: 4px !important;
+    height: 32px !important;
+    background: #bdc3c7;
+  }
+
+  /*
+Sidebar wrapper styles
+Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
+*/
+  .bm-menu-wrap {
+    position: fixed;
+    height: 100%;
+  }
+
+  /* General sidebar styles */
+  .bm-menu {
+    background: #373a47;
+    padding: 2.5em 1.5em 0;
+    font-size: 1.15em;
+  }
+
+  /* Morph shape necessary with bubble or elastic */
+  .bm-morph-shape {
+    fill: #373a47;
+  }
+
+  /* Wrapper for item list */
+  .bm-item-list {
+    color: #b8b7ad;
+    padding: 0.8em;
+  }
+
+  /* Individual item */
+  .bm-item {
+    display: inline-block;
+  }
+
+  /* Styling of overlay */
+  .bm-overlay {
+    background: rgba(0, 0, 0, 0.3);
+  }
+`
