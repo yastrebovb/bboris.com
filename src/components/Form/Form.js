2bb3a9fd629054b9a57f9af073f2871c7a2e5d79
Boris Yastrebov <mrhawkofficial@gmail.com>
2019-05-29 02:20:34 +0300

    form fix: post request data

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -30,7 +30,7 @@ class Form extends Component {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        contact: form.getAttribute("name"),
+        "form-name": form.getAttribute("name"),
         ...this.state,
       }),
     })
@@ -77,10 +77,8 @@ class Form extends Component {
 
     return (
       <FormStyled
-        form-name="contact"
         name="contact"
         method="post"
-        action="#"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         onSubmit={this.handleSubmit}
@@ -115,7 +113,6 @@ class Form extends Component {
           onChange={handleInput}
         />
         {getSubmitBtn()}
-        <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
       </FormStyled>
     )
